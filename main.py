53fbf4535de48dc585c3d4afc95837269bd8f711
Mustafa Cici <119680195+mustafacici1@users.noreply.github.com>
2025-01-24 16:13:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,4 @@ def download_video(url):
 # Örnek kullanım:
 video_url = input("url giriniz:")  # Buraya indirmek istediğiniz YouTube video linkini koyun
 download_video(video_url)
-import yt_dlp
 
-def download_video(url):
-    # Video indirme için ayarlar
-    ydl_opts = {
-        'format': 'best',  # En iyi kaliteyi seçer
-        'outtmpl': '%(title)s.%(ext)s',  # Videoyu mevcut dizine indirir ve video başlığını kullanarak kaydeder
-        'quiet': False  # Konsolda indirme süreci hakkında bilgi verir
-    }
-
-    # Video indirme işlemi
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-# Örnek kullanım:
-video_url = input("url giriniz:")  # Buraya indirmek istediğiniz YouTube video linkini koyun
-download_video(video_url)
